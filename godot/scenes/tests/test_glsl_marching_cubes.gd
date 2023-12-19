dcd1c553482afc12045947f374247bef564e065c
Mark McKay <mark@kitfox.com>
2023-12-18 23:22:33 -0500

    Adding button to export mesh.

--- godot/scenes/tests/test_glsl_marching_cubes.gd
+++ godot/scenes/tests/test_glsl_marching_cubes.gd
@@ -20,3 +20,23 @@ func _on_slider_threshold_value_changed(value):
 
 func _on_slider_resolution_value_changed(value):
 	%MarchingCubesGlsl.cube_resolution = value
+
+
+func _on_bn_save_pressed():
+	%save_dialog.popup_centered()
+
+
+func _on_save_dialog_file_selected(save_path:String):
+	var path:String = save_path
+	if !save_path.to_lower().ends_with(".gltf") && !save_path.to_lower().ends_with(".glb"):
+		path = save_path + ".glb"
+		
+		
+	var doc:GLTFDocument = GLTFDocument.new()
+	var state:GLTFState = GLTFState.new()
+	#var root:Node = plugin.get_editor_interface().get_edited_scene_root()
+	#var root_clean:Node3D = clean_flat(root) if %check_flatten.button_pressed else clean_branch(root)
+	
+	doc.append_from_scene(%MarchingCubesGlsl, state)
+	doc.write_to_filesystem(state, path)
+		
